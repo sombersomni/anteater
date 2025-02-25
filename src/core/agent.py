add end state for the queue for correct calculation

src/core/agent.py
@@ -139,7 +139,11 @@ class Agent:
             )
         )
 
-    def compute_rewards(self, win_state: bool = False):
+    def compute_rewards(
+        self,
+        win_state: bool = False,
+        lr=0.1
+    ):
         """
         Computes the rewards for the current queue of packets.
         We apply a time-based reward decay to the rewards in the queue.
@@ -152,6 +156,7 @@ class Agent:
         Returns:
             float: The total rewards for the queue
         """
+        logger.info(f"Agenet queue before computing rewards: {self.queue}")
         queue_length = len(self.queue)
         logger.info(f"Queue length: {queue_length}")
         time_decay = np.pow(
@@ -159,12 +164,11 @@ class Agent:
             2
         )
         rewards = np.array([packet.reward for packet in self.queue])
-        rewards += (1 if win_state else -1)
         rewards *= time_decay # Apply time-based reward decay
+        rewards += (1 if win_state else -1) * time_decay
         for idx, packet in enumerate(self.queue):
-            self._rewards_by_action_state[(packet.state, packet.action)] += rewards[idx]
-        total_rewards = np.sum(rewards).item()
-        logger.info(f"Total rewards: {total_rewards}")
+            self._rewards_by_action_state[(packet.state, packet.action)] += lr * rewards[idx]
+        total_rewards = np.sum(rewards).item() / queue_length
         self.clear_queue()
         return total_rewards
 

src/env_wrappers/path_finder.py
@@ -4,6 +4,10 @@ import numpy as np
 import math
 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
 class PathFinderRewardWrapper(Wrapper):
     def __init__(
         self,
@@ -26,13 +30,17 @@ class PathFinderRewardWrapper(Wrapper):
         row, col = obs % self.grid_size, obs // self.grid_size
         agent_current_position = np.array((row, col))
         agent_goal_position = np.array(self.goal_position)
-        vistor_reward = (-1 if obs in self._visited else 0)
+        vistor_reward = (0 if obs in self._visited else 0.1)
         distance = np.linalg.norm(agent_goal_position - agent_current_position)
         distance_reward = 1 / (distance + 1)
-        exploration_reward = self.max_steps - (len(self._visited) + 1)
+        exploration_reward = min(0, sigmoid(self.max_steps - (len(self._visited) + 1)))
         new_reward = reward + vistor_reward + distance_reward + exploration_reward
         self._visited.add(obs)
         info["win_state"] = (
             obs == self.goal_position[0] * self.grid_size + self.goal_position[1]
         )
         return obs, new_reward, terminated, truncated, info
+
+    def reset(self):
+        self._visited.clear()
+        return self.env.reset()